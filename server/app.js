39c6b7f5ccfff9be45f0c7979c9239c5624e30a0
dany92 <dany@bc.edu>
2016-08-11 17:55:32 -0400

    initialize with python

---
+++ server/app.js
@@ -0,0 +1,40 @@
+var Express = require('express');
+var child_process = require('child_process');
+var path = require('path');
+var app = Express(); // Create an express app!
+var spawn = child_process.spawn;
+var py = spawn('python', ['server/testing.py']);
+module.exports = app;
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var indexHtmlPath = path.join(__dirname, '../index.html');
+
+app.get('/', function (req, res) {
+    res.sendFile(indexHtmlPath);
+});
+
+app.post('/train', function(req,res,next){
+	var trainingData = req.body;
+	var input = trainingData.input;
+	var output = trainingData.output;
+	var data = [input, output];
+	var finalArr = [];
+
+	py.stdout.on('data', function(data){
+	  finalArr.push(data);
+	});
+
+	py.stdout.on('end', function(){
+	  res.send(finalArr); //sends a buffer of arrays need to do res.data to retrieve
+	  next();
+	});
+
+	py.stdin.write(JSON.stringify(data));
+
+	py.stdin.end();
+})
+
+//{"input":"[[1,2],[3,4]]", "output":"[[5,6],[7,8]]"}
---
+++ server/index.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var server = http.createServer();
+
+// Require our express app from the app.js file.
+var app = require('./app');
+
+// Every server request runs through our express app!
+server.on('request', app);
+
+// Export our server for this file to be require('')d
+server.listen(1337,function(){
+	console.log("listening");
+})
