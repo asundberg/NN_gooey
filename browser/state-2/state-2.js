871e213a297d36702e2a4b92fbea9cbb0d040d7a
Annika Sundberg <annika.m.h.sundberg@gmail.com>
2016-08-15 16:57:08 -0400

    Added functionality to trainer-factory and state-2.js

--- browser/state-2/state-2.js
+++ browser/state-2/state-2.js
@@ -5,18 +5,35 @@ app.config(function ($stateProvider) {
   $stateProvider.state('state2', {
     url: '/train',
     templateUrl: '/state-2/template.html',
-    controller: 'State2Ctrl'
-    // resolve: {
-    //   trainer: function (TrainerFactory) {
-    //     return TrainerFactory.get();
-    //   }
-    // }
+    controller: 'State2Ctrl',
+    resolve: {
+      inputs: function (TrainerFactory) {
+        return TrainerFactory.getInputs();
+      }
+    }
   });
 });
 
-app.controller('State2Ctrl', function ($scope, TrainerFactory) {
+app.controller('State2Ctrl', function ($scope, TrainerFactory, inputs) {
 
-  // $scope. = ;
   d3.selectAll("p").style("color", "blue");
+
+  $scope.numInputs = inputs;
+
+  $scope.hiddenLayers = 1;
+
+  $scope.trainNetwork = function () {
+    $scope.greeting = 'The network is being trained!!!';
+  };
+
+  $scope.addLayers = function () {
+    $scope.hiddenLayers++;
+  };
+
+  $scope.removeLayers = function () {
+    if ($scope.hiddenLayers >= 0) {
+      $scope.hiddenLayers--;
+    }
+  };
 
 });
