c0e05fc66150ef5dacc70abd6c37829aed13ac96
Annika Sundberg <annika.m.h.sundberg@gmail.com>
2016-08-18 12:03:02 -0400

    Added loader

--- browser/trainer-factory.js
+++ browser/trainer-factory.js
@@ -7,8 +7,7 @@ app.factory('TrainerFactory', function ($http) {
       inputArr: null,
       outputArr: null,
       headerReference: null,
-      hiddenLayersArr: [], // array of numbers, each indicating number of neurons in that layer
-      resultObj: null
+      hiddenLayersArr: [] // array of numbers, each indicating number of neurons in that layer
     };
 
     TrainerFactory.setData = function(data){
@@ -22,7 +21,8 @@ app.factory('TrainerFactory', function ($http) {
       console.log('The network is being trained! (Cool graphic is showing...)', TrainerFactory);
       return $http.post('/train', dataObj)
       .then(function (response) {
-        this.resultObj = response.data;
+        var resultObj = response.data;
+        return resultObj;
       });
     };
 
