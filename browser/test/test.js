ad3f9f438b6ba7afd2648e21e7965a5a1c3c254e
dany92 <dany@bc.edu>
2016-08-20 19:22:38 -0400

    my changes

--- browser/test/test.js
+++ browser/test/test.js
@@ -2,13 +2,13 @@
 
 app.config(function ($stateProvider) {
   $stateProvider.state('test', {
-    url: '/test',
+    url: '/test/:id',
     templateUrl: '/test/template.html',
     controller: 'TestCtrl'
   });
 });
 
-app.controller('TestCtrl', function ($scope) {
+app.controller('TestCtrl', function ($scope, $http, $stateParams) {
 
   // $scope.testInputs = function () {
 
@@ -17,6 +17,19 @@ app.controller('TestCtrl', function ($scope) {
   // $scope.testData = function () {
 
   // };
+  
+  // for fetching the sample rows for inputs
+  // $http.get('/selections/'+$stateParams.id)
+  // .then(sampleInput=>{
+  // 	$scope.sample = sampleInput
+  // })
 
+  $scope.test = function(){
+  	$http.post('/test/'+$stateParams.id,{})
+  	.then(res =>{
+  		var response = res.data;
+  		console.log(res.data);
+  	})
+  }
 
 });
--- testnumpy.py
+++ testnumpy.py
@@ -2,15 +2,12 @@ import numpy
 import pandas
 import math
 import json
-# from encoder import Encoder as LabelEncoder
-# from encoder import Decoder
+from encoder import Encoder as LabelEncoder
+from encoder import Decoder
 from keras.utils import np_utils
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.models import model_from_json
-
-def getData():
-	return [1.0,2.0,3.0,4.0,5.1,6.1,7.1,8.1,9.1,10.2,0.94,0.99]
 
 def trainModel():
 	testList = {
@@ -35,7 +32,7 @@ def trainModel():
 
 	#initializations
 	# testType ='multi_class'
-	testType = 'binary'
+	testType = 'multi_class'
 	currentTest=testList[testType]; #from param
 	initWgt = 'uniform'
 	initAct = 'relu'
@@ -43,14 +40,84 @@ def trainModel():
 	initLoss=''
 	initDelimiter=",";
 	# filename = "/Users/Ddangu/nn_gooey_practice/datasets/soybean-small.data.txt"
-	X = numpy.array([[1,2,3,4,5],[11,12,13,14,15],[21,22,23,24,25]])
-	# filename="/Users/Ddangu/nn_gooey_practice/datasets/pima-indians-diabetes.txt"
-	# dataframe = pandas.read_csv(filename, sep=initDelimiter, header=None)
-	# dataset = dataframe.values
-	# shape = dataframe.shape
-	# numX = shape[1]
-	# numY = shape[0]
-	# numInputs = numX-1
-	print pandas.DataFrame(X).shape
-	
+	filename="/Users/Ddangu/nn_gooey_practice/datasets/iris2.txt"
+	dataframe = pandas.read_csv(filename, sep=initDelimiter, header=None)
+	dataset = dataframe.values
+	shape = dataframe.shape
+	numX = shape[1]
+	numY = shape[0]
+	numInputs = numX-1
+	X = dataset[:,0:numInputs].astype(float)
+	Y = dataset[:,numInputs]
+	encoder = LabelEncoder()
+	encoded_Y = encoder.transform(Y)
+	currentTest['outputY'] = encoded_Y
+
+	#need the following line for multi-class
+	if testType=='multi_class':
+		dummy_y = np_utils.to_categorical(encoded_Y)
+		currentTest['outputY']=dummy_y
+		currentTest['outputNum']=dummy_y.shape[1]
+
+	def create_baseline():
+		# create model
+		model = Sequential()
+		model.add(Dense(numInputs, input_dim=numInputs, init=initWgt, activation=initAct))
+		model.add(Dense(currentTest['outputNum'], init=initWgt, activation=currentTest['outputAct']))
+		# Compile model
+		model.compile(loss=currentTest['loss'], optimizer=initOptimizer, metrics=['accuracy'])
+		return model
+
+	model = create_baseline()
+	history = model.fit(X, currentTest['outputY'], batch_size=150, nb_epoch=100, verbose=0)
+	results = model.evaluate(X, currentTest['outputY'], verbose=0)
+	print('Test score:', results[0])
+	print('Test accuracy:', results[1])
+	# print(history.history['acc']) #loss and accuracy per epoch
+
+	decoder = Decoder()
+	decoder.setDecoder(encoder.getKeyVal())
+
+	lib = {
+		'lossType': currentTest['loss'],
+		'decoder': decoder.getDecoder()
+	}
+
+	with open("lib.json", 'w') as json_data:
+	    json.dump(lib, json_data)
+
+	model_json = model.to_json()
+	with open("model.json", "w") as json_file:
+	    json_file.write(model_json)
+	# serialize weights to HDF5
+	model.save_weights("model.h5")
+	print("Saved model to disk")
+
+	score = model.predict(X, batch_size=150, verbose=0)
+
+	#if multi_class
+	def find_max(arr):
+		maxIndex = numpy.argmax(arr)
+		return maxIndex
+
+	def find_binary(arr):
+		if(arr[0]>0.5):
+			return 1
+		return 0
+
+	def decodePredictedOutput(arr):
+		if(testType == 'multi_class'):
+			arr = map(find_max, arr)
+		else: 
+			arr = map(find_binary, arr)
+		return map(decoder.decode, arr)
+
+	predictedScore = decodePredictedOutput(score)
+
+	sendBack = {
+		'accuracy': history.history['acc'],
+		'predicted': predictedScore
+	}
+
+	print sendBack
 trainModel()
