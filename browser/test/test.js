c82b2b6b8524ab4480c1dde00ee024ab9a23b7fb
dany92 <dany@bc.edu>
2016-08-21 14:18:37 -0400

    frontend for testState

--- browser/test/test.js
+++ browser/test/test.js
@@ -14,26 +14,27 @@ app.config(function ($stateProvider) {
   });
 });
 
-//Retrieve Selections from Selections model 
 app.controller('TestCtrl', function ($scope, $http, $stateParams) {
-	$scope.sampleHeaders = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6','col7','col8'];
-	$scope.sampleRows = [[6,148,72,35,0,33.6,0.627,50],[1,85,66,29,0,26.6,0.351,31]];
-	$scope.tabs = [{name: 'Single Input', url: "/test/single.html"},{name:'Multiple Input', url: '/test/multiple.html'}];
-	
-  	$scope.testInputs = {}
-
   // for fetching the sample rows for inputs
   // $http.get('/selections/'+$stateParams.id)
   // .then(sampleInput=>{
-  // 	$scope.sample = sampleInput
+  //  $scope.sample = sampleInput
   // })
+	$scope.sampleHeaders = ['col1', 'col2', 'col3', 'col4', 'col5', 'col6','col7','col8'];
+	$scope.sampleRows = [[6,148,72,35,0,33.6,0.627,50],[1,85,66,29,0,26.6,0.351,31]];
+	$scope.tabs = [{name: 'Single Input', url: "/test/single.html"},{name:'Multiple Input', url: '/test/multiple.html'}];
+	 
+  $scope.test = {}; //in case you need other properties
+  $scope.test.testInputs = [];
 
-	$scope.test = function(){
-		$http.post('/test/'+$stateParams.id,{})
-		.then(res =>{
-			var response = res.data;
-			console.log(res.data);
-		})
+	$scope.submit = function(){
+    console.log($scope.test.testInputs)
+
+		// $http.post('/test/'+$stateParams.id, $scope.test)
+		// .then(res =>{
+		// 	var response = res.data;
+		// 	console.log(response);
+		// })
 	}
 
 });
