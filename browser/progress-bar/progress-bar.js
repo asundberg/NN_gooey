08be8088b2cb35bdf61bf9a55340daef07f40f47
Winnie Chen <wchen0@gmail.com>
2016-08-22 02:05:38 -0400

    click event holders for progressbar

--- browser/progress-bar/progress-bar.js
+++ browser/progress-bar/progress-bar.js
@@ -1,8 +1,15 @@
-app.directive('progressBar', function () {
+app.directive('progressBar', function ($state) {
     return {
         restrict: 'E',
+        scope: {},
         templateUrl: '/progress-bar/progress-bar.html',
         link: function (scope, element, attrs) {
+            scope.state1 = function() {
+                $state.go('upload');
+            }
+            scope.state2 = function() {
+                $state.go('state2');
+            }
         }
     };
 });
